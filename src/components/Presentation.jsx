f7efd182e3c12a2395da99237ddd9eba996c2954
so265 <undia42@gmail.com>
2023-11-16 19:54:33 +0100

    presentation

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import "../styleComponents/Presentation.scss";
 
 
 function Presentation() {
   return (
-    <div id='presentation'>Presentation, je suis developpeuse web frontend
+    <div id='presentation'><h1>Je suis une développeuse web frontend</h1> 
+    <p>J'aime concevoir des sites web à la fois performants et attrayants.</p> 
+    <p>Mon objectif est de créer des sites de qualité en suivant les dernières tendances du secteur.</p>
    
     </div>
   )
