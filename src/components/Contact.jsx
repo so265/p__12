610428d03c163695dca94c84555f2b31cfa0ffd3
so265 <undia42@gmail.com>
2023-11-19 17:42:56 +0100

    add arrow

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "../styleComponents/Contact.scss";
+import ScrollToTop from './ScrollToTop';
 
 function Contact() {
   return (
     <div>
         <h1 id='contact'>Pour me contacter</h1>
         <p className='whrite-me'><a href="mailto:undia42@gmail.com">Whrite-me</a></p>
+        <ScrollToTop />
         </div>
   )
 }
---
+++ src/components/ScrollToTop.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import "../styleComponents/ScrollToTop.scss";
+
+const ScrollToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Afficher le bouton lorsque l'utilisateur défile vers le bas de 500 pixels
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 500) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  // Faire défiler jusqu'en haut de la page
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+
+  return (
+    <div className="scroll-to-top">
+      {isVisible && (
+        <div onClick={scrollToTop} className="arrow-up">
+          <i className="fas fa-arrow-up"></i>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ScrollToTop;
